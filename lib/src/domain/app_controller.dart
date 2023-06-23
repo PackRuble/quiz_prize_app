b032622689bcee86eb5b745056943ca6f2f00b89
Ruble <pack.ruble@gmail.com>
2023-06-23 10:32:57 +0300

    upd: separated `AppProvider` from `AppBloc`

--- lib/src/domain/app_controller.dart
+++ lib/src/domain/app_controller.dart
@@ -2,16 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:trivia_app/src/data/local_storage/app_storage.dart';
 
-class AppController {
-  AppController._({
-    required AppStorage appStorage,
-  }) : _appStorage = appStorage;
+class AppProvider extends AppBloc {
+  AppProvider._({required super.appStorage});
 
   static final instance = AutoDisposeProvider(
-    (ref) => AppController._(
+    (ref) => AppProvider._(
       appStorage: ref.watch(AppStorage.instance),
     ),
   );
+
+  late final themeMode = AutoDisposeProvider(
+    (ref) => _appStorage.attach(
+      AppCard.themeMode,
+      (value) => ref.state = value,
+      detacher: ref.onDispose,
+    ),
+  );
+
+  late final themeColor = AutoDisposeProvider(
+    (ref) => _appStorage.attach(
+      AppCard.themeColor,
+      (value) => ref.state = value,
+      detacher: ref.onDispose,
+    ),
+  );
+}
+
+class AppBloc {
+  AppBloc({
+    required AppStorage appStorage,
+  }) : _appStorage = appStorage;
 
   final AppStorage _appStorage;
 
@@ -25,27 +45,11 @@ class AppController {
   // ***************************************************************************
   // theme mode
 
-  late final themeMode = AutoDisposeProvider(
-    (ref) => _appStorage.attach(
-      AppCard.themeMode,
-      (value) => ref.state = value,
-      detacher: ref.onDispose,
-    ),
-  );
-
   Future<void> selectThemeMode(ThemeMode mode) async =>
       _appStorage.set<ThemeMode>(AppCard.themeMode, mode);
 
   // ***************************************************************************
   // theme color
-
-  late final themeColor = AutoDisposeProvider(
-    (ref) => _appStorage.attach(
-      AppCard.themeColor,
-      (value) => ref.state = value,
-      detacher: ref.onDispose,
-    ),
-  );
 
   Future<void> selectThemeColor(Color color) async =>
       _appStorage.set<Color>(AppCard.themeColor, color);
